d79bf5b17181f1460d6ffe2b0742da9931e3967b
CJMyers1 <myerscasper73@gmail.com>
2022-02-10 08:57:49 -0700

    Object Functions

--- Object functions/Functional Programming/app.js
+++ Object functions/Functional Programming/app.js
@@ -18,7 +18,7 @@ var arr2 = mapForEach(arr1, function(item) {
 });
 console.log(arr2);
 
-var arr3 = mapForEach(arr2, function(item) {
+var arr3 = mapForEach(arr1, function(item) {
     return item > 2;
 });
 console.log(arr3);
