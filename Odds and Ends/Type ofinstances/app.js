84001afbd4dd0df76cfa44c9cc1f43979edeb5b2
CJMyers1 <myerscasper73@gmail.com>
2022-02-10 10:04:57 -0700

    Odds and Ends

---
+++ Odds and Ends/Type ofinstances/app.js
@@ -0,0 +1,26 @@
+var a = 3;
+console.log(typeof a);
+
+var b = "Hello";
+console.log(typeof b);
+
+var c = {};
+console.log(typeof c);
+
+var d = [];
+console.log(typeof d); // its an object?? should be array
+console.log(Object.prototype.toString.call(d));
+
+function Person(name) {
+    this.name = name;
+}
+
+var e = new Person('Jane');
+console.log(typeof e);
+console.log(e instanceof Person);
+
+console.log(typeof undefinded);
+console.log(typeof null);
+
+var z = function() { };
+console.log(typeof z);
